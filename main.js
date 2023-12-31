62cdcca6268beb16bf11a22add7225b7dbea3176
babytayo <121583836+babytayo@users.noreply.github.com>
2023-12-31 13:41:14 +0100

    used object data store to arrange my event handler

--- main.js
+++ main.js
@@ -1,71 +1,71 @@
 {
   const Switch = document.querySelector(".switch");
   Switch.addEventListener("click", () => {
+    // Object Data store
     const move = document.querySelector(".toggle");
+    const changeColor = {
+      outputArea: document.querySelector(".output_area"),
+      btnBox: document.querySelector(".btn_box"),
+      headSection: document.querySelector(".head_section"),
+      allBtn: document.querySelectorAll(".btn"),
+      specialBtn1: document.querySelector(".special_btn1"),
+      specialBtn2: document.querySelectorAll(".special_btn2"),
+    };
+    // Object Data store
+
     if (move.style.left === "0%") {
+      // Toggle btn movement
       move.style.left = "40%";
+
+      // Change cutom CSS Colours
       move.style.background = "#c85401";
       document.body.style.background = "#e6e6e6";
-      document.querySelector(".output_area").style.background = "#eeeeee";
-      document.querySelector(".output_area").style.color = "#35352b";
-
-      document.querySelector(".btn_box").style.background = "#d3cdcd";
-      document.querySelector(".head_section").style.color = "#35352b";
-      document
-        .querySelectorAll(".btn")
-        .forEach((e) => (e.style.color = "#35352b"));
-      document
-        .querySelectorAll(".btn")
-        .forEach((e) => (e.style.background = "#e5e4e0"));
-      document.querySelector(".special_btn1").style.background = "#c85401";
-      document
-        .querySelectorAll(".special_btn2")
-        .forEach((e) => (e.style.background = "#388187"));
-      document.querySelector(".special_btn1").style.color = "#eae3db";
+      changeColor.outputArea.style.background = "#eeeeee";
+      changeColor.outputArea.style.color = "#35352b";
+      changeColor.btnBox.style.background = "#d3cdcd";
+      changeColor.headSection.style.color = "#35352b";
+      changeColor.allBtn.forEach((e) => (e.style.color = "#35352b"));
+      changeColor.allBtn.forEach((e) => (e.style.background = "#e5e4e0"));
+      changeColor.specialBtn1.style.background = "#c85401";
+      changeColor.specialBtn1.style.color = "#eae3db";
+      changeColor.specialBtn2.forEach((e) => (e.style.background = "#388187"));
 
       !important;
     } else if (move.style.left === "40%") {
+      // Toggle btn movement
       move.style.left = "80%";
+
+      // Change cutom CSS Colours
       move.style.background = "#00decf";
       document.body.style.background = "#17062a";
-      document.querySelector(".output_area").style.background = "#1e0836";
-      document.querySelector(".output_area").style.color = "#d8c254";
-
-      document.querySelector(".btn_box").style.background = "#1e0836";
-      document.querySelector(".head_section").style.color = "#d8c254";
-      document
-        .querySelectorAll(".btn")
-        .forEach((e) => (e.style.color = "#d8c254"));
-      document
-        .querySelectorAll(".btn")
-        .forEach((e) => (e.style.background = "#331b4d"));
-      document.querySelector(".special_btn1").style.background = "#00decf";
-      document.querySelector(".special_btn1").style.color = "#000000";
-      document
-        .querySelectorAll(".special_btn2")
-        .forEach((e) => (e.style.background = "#56077c"));
+      changeColor.outputArea.style.background = "#1e0836";
+      changeColor.outputArea.style.color = "#d8c254";
+      changeColor.btnBox.style.background = "#1e0836";
+      changeColor.headSection.style.color = "#d8c254";
+      changeColor.allBtn.forEach((e) => (e.style.color = "#d8c254"));
+      changeColor.allBtn.forEach((e) => (e.style.background = "#331b4d"));
+      changeColor.specialBtn1.style.background = "#00decf";
+      changeColor.specialBtn1.style.color = "#000000";
+      changeColor.specialBtn2.forEach((e) => (e.style.background = "#56077c"));
       !important;
     } else {
+      // Toggle btn movement
       move.style.left = "0%";
+
+      // Change cutom CSS Colours
       move.style.background = "#d03e31";
       document.querySelector(".special_btn1").style.background = "#d03e31";
       document.body.style.background = "#3b4664";
-      document.querySelector(".output_area").style.background = "#181f32";
-      document.querySelector(".output_area").style.color = "#fbfbfb";
-
-      document.querySelector(".btn_box").style.background = "#181f32";
-      document.querySelector(".head_section").style.color = "#e1e4e9";
-      document
-        .querySelectorAll(".btn")
-        .forEach((e) => (e.style.color = "#3b4664"));
+      changeColor.outputArea.style.background = "#181f32";
+      changeColor.outputArea.style.color = "#fbfbfb";
+      changeColor.btnBox.style.background = "#181f32";
+      changeColor.headSection.style.color = "#e1e4e9";
+      changeColor.allBtn.forEach((e) => (e.style.color = "#3b4664"));
+      changeColor.allBtn.forEach((e) => (e.style.background = "#eae3db"));
+      changeColor.specialBtn1.style.background = "d03e31";
+      changeColor.specialBtn1.style.color = "#eae3db";
+      changeColor.specialBtn2.forEach((e) => (e.style.background = "#647299"));
+      !important;
     }
-    document
-      .querySelectorAll(".btn")
-      .forEach((e) => (e.style.background = "#eae3db"));
-    document
-      .querySelectorAll(".special_btn2")
-      .forEach((e) => (e.style.background = "#647299"));
-    document.querySelector(".special_btn1").style.color = "#eae3db";
-    !important;
   });
 }
